f26fc6261d2ee9fc09a6efbb3d64c4d489288e4e
Titus Njema <njema3@gmail.com>
2024-07-20 13:02:06 +0300

    first commit

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -4,6 +4,7 @@ const Task = ({ todo, handleDelete }) => {
   return (
     <div className='task'>        
       <li>{todo}</li>
+      <button>Edit</button>
       <button onClick={() => handleDelete(todo)}>Delete</button>
     </div>
   )
